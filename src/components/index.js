04a2cc31979aa8af44b24146e93527add2e19313
JulioGrcSrr <73603634+JulioGrcSrr@users.noreply.github.com>
2021-05-17 18:25:19 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,6 @@ export {default as SigninButton} from "./signinbutton";
 export {default as Comment} from "./comment";
 export {default as CommentInput} from "./comment-input";
 export {default as Like} from "./like";
+export {default as Message} from "./message";
+export {default as CreateMessage} from "./create-message";
+export {default as LogoutButton} from "./logoutbutton";
