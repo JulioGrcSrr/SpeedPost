e8264d1264f4cd2c77aa23f2d128348b06465d35
JulioGrcSrr <73603634+JulioGrcSrr@users.noreply.github.com>
2021-05-17 18:22:54 +0200

    Update index.js

--- src/containers/index.js
+++ src/containers/index.js
@@ -2,3 +2,5 @@ export {default as Navbar} from './navbar';
 export {default as CreatePost} from './create-post';
 export {default as PopUp} from './popup';
 export {default as Post} from './post';
+export {default as CreateRoom} from './create-room';
+export {default as Room} from './rooms';
