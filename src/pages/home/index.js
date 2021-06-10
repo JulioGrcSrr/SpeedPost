bd61937c570a3a6aaf7ea39277e31d0f6efa968f
JulioGrcSrr <73603634+JulioGrcSrr@users.noreply.github.com>
2021-06-10 18:30:01 +0200

    Add files via upload

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,27 +1,34 @@
 import React, {useContext , useState} from 'react'
-import { CreatePost, Navbar, PopUp } from '../../containers'
+import { CreatePost, Navbar, PopUp , CreateRoom } from '../../containers'
 import Feed from '../../containers/feed';
+import FeedRoom from '../../containers/feed-room';
 import { UserContext } from '../../contexts/user'
 import './style.css';
 export default function Home() {
     const [user, setUser] = useContext(UserContext).user
     const [layout, setLayout] = useState(false);
+    const [site, setSite] = useState('Rooms');
 
     const changeLayout = (() =>{
         if(layout){
             setLayout(false)
+            setSite('Rooms')
         }else{
             setLayout(true)
+            setSite('Post')
         }
     })
 
     return (
         <div className="home">
+          
             <Navbar />
-            <div >
+            <div className="homeLayout">
             {/* check if the user is logged */}
-            {user ? <button onClick={changeLayout}>Hola</button> : <PopUp /> }
-            {layout ? <div> work </div> : <div className="homeBox">
+            {user ? <button onClick={changeLayout}className="siteBtn" >{site}</button> : <PopUp /> }
+            {layout ? <div className="homeBox"> {user ? <CreateRoom /> : <p></p>} {user ? <FeedRoom/> : <p></p>} </div> : 
+            
+            <div className="homeBox">
                 {user ? <CreatePost /> : <p></p>} 
                 
                 {user ? <Feed /> : <p></p>}
